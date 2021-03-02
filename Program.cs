f43b0421357c1178c321655f70605b81ad61002f
anna-moore <ammoore434@gmail.com>
2021-03-02 10:03:29 -0600

    phase one

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+// Display a message to the user asking them to guess the secret number.
+// Display a prompt for the user's guess.
+// Take the user's guess as input and save it as a variable.
+// Display the user's guess back to the screen.
+
+
+Console.WriteLine("Welcome to the Guessing Game!");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine("Would you like to guess the secret number?");
+Console.Write("Your Guess: ");
+string answer = Console.ReadLine();
+int parsedAnswer = Int32.Parse(answer);
+Console.Write(answer);
