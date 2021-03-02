9a2227d71951985b2b2306714c1fc61b56883011
anna-moore <ammoore434@gmail.com>
2021-03-02 13:46:15 -0600

    final phases

--- Program.cs
+++ Program.cs
@@ -6,44 +6,44 @@
 // Take the user's guess as input and save it as a variable.
 // Display the user's guess back to the screen.
 
-
-Console.WriteLine("Welcome to the Guessing Game!");
-Console.WriteLine("--------------------------------------------");
-Console.WriteLine("Would you like to guess the secret number (1-100)?");
-
-Console.Write("Your Guess: ");
-string answer = Console.ReadLine();
-int parsedAnswer = Int32.Parse(answer);
-
-//Phase Two
-//The program should be updated to...
-// Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
-// No longer display the user's guess. They know what they guessed, right?
-// Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
-
-//Phase Three
-//The program should be updated to...
-//Give the user four chances to guess the number.
-//Continue to display the success or failure messages as in phase 2
-
-//Phase Four 
-//show the number of tries 
-//end the loop **
-//error the correct display on the last try doesn't show 
-
-// Phase 5
-// Use a random number between 1 and 100 instead of a hard-coded number.
-// The prompt should display the number of guesses the user has left.
-
-//Phase 6 
-// Inform the user if their guess was too high or too low, when they guess incorrectly.
-
 //Phase 7
 // Prompt the user for a difficulty level before they are prompted to guess the number.
 // The difficulty level should determine how many guesses the user gets. The difficulty levels should be:
 // Easy - this gives the user eight guesses.
 // Medium - this gives the user six guesses.
 // Hard - this gives the user four guesses.
+
+Console.WriteLine("Welcome to the Guessing Game!");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine("What difficulty level would you like to play on?");
+Console.Write(" Enter E for Easy, M for Medium, H for Hard, C for Cheater: ");
+string level = Console.ReadLine().ToLower();
+int triesPerLevel = 0;
+
+if (level == "h")
+{
+    triesPerLevel = 3;
+}
+else if (level == "m")
+{
+    triesPerLevel = 5;
+}
+else if (level == "e")
+{
+    triesPerLevel = 7;
+}
+else if (level == "c")
+{
+    triesPerLevel = 100;
+}
+
+
+
+Console.WriteLine("Would you like to guess the secret number (1-100)?");
+
+Console.Write("Your Guess: ");
+string answer = Console.ReadLine();
+int parsedAnswer = Int32.Parse(answer);
 
 int RandomNumber()
 {
@@ -53,7 +53,7 @@ int RandomNumber()
     return genRand;
 }
 int secretNumber = RandomNumber();
-for (int numOfTries = 3; numOfTries > -1; numOfTries--)
+for (int numOfTries = triesPerLevel; numOfTries > -1; numOfTries--)
 {
     if (parsedAnswer == secretNumber)
     {
@@ -77,3 +77,27 @@ for (int numOfTries = 3; numOfTries > -1; numOfTries--)
     answer = Console.ReadLine();
     parsedAnswer = Int32.Parse(answer);
 }
+//Phase Two
+//The program should be updated to...
+// Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
+// No longer display the user's guess. They know what they guessed, right?
+// Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
+
+//Phase Three
+//The program should be updated to...
+//Give the user four chances to guess the number.
+//Continue to display the success or failure messages as in phase 2
+
+//Phase Four 
+//show the number of tries 
+//end the loop **
+//error the correct display on the last try doesn't show 
+
+// Phase 5
+// Use a random number between 1 and 100 instead of a hard-coded number.
+// The prompt should display the number of guesses the user has left.
+
+//Phase 6 
+// Inform the user if their guess was too high or too low, when they guess incorrectly.
+
+
