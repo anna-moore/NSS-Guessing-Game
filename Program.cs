20e79a0e117b0a856ef9d33a3e1f6f85918bc0e8
anna-moore <ammoore434@gmail.com>
2021-03-02 10:08:35 -0600

    phase two: compare numbers

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-
+//Phase One
 // Display a message to the user asking them to guess the secret number.
 // Display a prompt for the user's guess.
 // Take the user's guess as input and save it as a variable.
@@ -13,4 +13,20 @@ Console.WriteLine("Would you like to guess the secret number?");
 Console.Write("Your Guess: ");
 string answer = Console.ReadLine();
 int parsedAnswer = Int32.Parse(answer);
-Console.Write(answer);
+
+//Phase Two
+//The program should be updated to...
+// Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
+// No longer display the user's guess. They know what they guessed, right?
+// Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
+
+int secretNumber = 36;
+
+if (parsedAnswer == secretNumber)
+{
+    Console.WriteLine("the guess is correct");
+}
+else
+{
+    Console.WriteLine("otherwise display a failure message.");
+}
